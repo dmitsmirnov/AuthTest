22bc3695e9d2603c2bec2e44bbd716ef570eda40
dmitsmirnov <dmitsmirnov@bk.ru>
2024-09-20 13:49:39 +0300

    add new project

---
+++ AuthTest/AuthTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTestApp.swift
+//  AuthTest
+//
+//  Created by dmitsmirnov on 20.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
